e148aa9e97f557c2801749265a99f27f31333e26
jhonattanRamon <jhonattan.ramon.17@gmail.com>
2023-05-25 15:33:39 -0300

    .

--- api/spotify/auth.js
+++ api/spotify/auth.js
@@ -18,7 +18,7 @@ var { default: axios } = require("axios");
 
 var client_id = process.env.CLIENT_ID_SPOTIFY; // Your client id
 var client_secret = process.env.CLIENT_SECRET_SPOTIFY; // Your secret
-var redirect_uri = process.env.REDIRECT_URI; // Your redirect uri
+var redirect_uri = process.env.REDIRECT_URI_PRODUCT; // Your redirect uri
 
 var baseURlServer = "https://appnative-backend.onrender.com";
 /**
