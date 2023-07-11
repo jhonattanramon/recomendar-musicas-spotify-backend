3965e8f847423f5c3fbd3221d64db7f45a912df6
jhonattanRamon <jhonattan.ramon.17@gmail.com>
2023-07-11 19:11:20 -0300

    .

--- api/User.js
+++ api/User.js
@@ -1,8 +1,8 @@
 class User {
-  constructor(access_token, refresh_token, data_user) {
-    this.access_token = access_token,
-    this.refresh_token = refresh_token,
-    this.dataUser = data_user,
+  constructor() {
+    this.access_token = null,
+    this.refresh_token = null,
+    this.dataUser = null,
     this.response = null,
     this.infor = null
   }
